58f9d42ab34afc7075ae20df39e96965a43fe88a
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-09 21:41:46 +0530

    another1

--- python4.py
+++ python4.py
@@ -4,3 +4,8 @@ def is_prime(num):
     for i in range(2, int(num**0.5)+1):
         if num % i == 0:
             return False
+        return True
+
+start = int(input("Enter start of range: "))
+end = int(input("Enter end of range: "))
+
