fd8ed3934ccaff24ffccaf5a2304736b49d8e428
sravan-0110 <chitra.kumar2@s.amity.edu>
2025-07-10 23:36:02 +0530

    main1

--- python5.py
+++ python5.py
@@ -2,3 +2,6 @@ def divide_8_digit_numbers(num1, num2):
     if len(str(num1)) != 8 or len(str(num2)) != 8:
          print("Both numbers must be 8 digits long.")
         return
+    if num2 == 0:
+        print("Error: Division by zero is not allowed.")
+        return
